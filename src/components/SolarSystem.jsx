7ca9d5a2466c23d27e996eb5e4ef6105ecb118a5
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-25 17:35:02 -0300

    2. Crie um componente chamado SolarSystem- 3. Crie um componente chamado Title

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Title from './Title';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system"> </div>
+    );
+  }
+}
+export default SolarSystem;
