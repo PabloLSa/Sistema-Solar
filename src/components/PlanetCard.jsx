79320f754f3ba97f7e0a930983576663f23aa6cd
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-25 18:53:59 -0300

    5. Crie um componente chamado PlanetCard

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+
+    );
+  }
+}
+PlanetCard.propTypes = {
+  planetImage: PropTypes.string,
+  planetName: PropTypes.string,
+}.isRequired;
+export default PlanetCard;
